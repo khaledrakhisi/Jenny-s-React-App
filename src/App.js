625882e917355bea6f03e2fec12514c882660a2a
khaledr <info@khaledr.ir>
2022-08-09 21:51:44 +0430

    Bad patterns and strange choices spotted

--- src/App.js
+++ src/App.js
@@ -1,71 +1,144 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import { addWatchedMovie, add, removeWatchedMovie, getWatchedMovies, getAllMovies } from './index.js';
+import {
+  addWatchedMovie,
+  add,
+  removeWatchedMovie,
+  getWatchedMovies,
+  getAllMovies,
+} from "./index.js";
 
+/**
+ *
+ * React components should be stored in seperate folder called 'components'
+ */
 const getMoviesComponents = (movies) => {
   var components = [];
 
-  movies.forEach(function(movie) {
+  movies.forEach(function (movie) {
     components.push(
       <div className="all">
         <div>
+          {/* Every img tag should have a valid alt attribute value, describing the image for accessibility sake*/}
           <img src={movie.image} height="100px" />
         </div>
         <span>
-          <a className="movie-watched" href="#" onClick={function() { addWatchedMovie(movie.title, movie.comment, movie.image) }}>
+          {/* Every a tag should have a valid href value */}
+          <a
+            className="movie-watched"
+            href="#"
+            onClick={function () {
+              addWatchedMovie(movie.title, movie.comment, movie.image);
+            }}
+          >
             {movie.title}
           </a>
         </span>
         <br />
-        <span>
-          {movie.comment}
-        </span>
+        <span>{movie.comment}</span>
         <br />
         <br />
       </div>
-    )
-  })
+    );
+  });
 
   return components;
-}
+};
 
+/**
+ * DRY or Don't Repeat Yourself
+ * These two components render a same html so they should be definded as one
+ *
+ */
 function getWatchedMoviesComponents(movies) {
   var components = [];
 
-  movies.forEach(function(movie) {
-    components.push(movie && (
-      <div className="watched">
-        <div>
-          <img src={movie.image} height="100px" />
+  movies.forEach(function (movie) {
+    components.push(
+      movie && (
+        <div className="watched">
+          <div>
+            {/* Every img tag should have a valid alt attribute value, describing the image for accessibility sake*/}
+            <img src={movie.image} height="100px" />
+          </div>
+          <span>
+            {/* Every a tag should have a valid href value */}
+            <a
+              className="movie-watched"
+              href="#"
+              onClick={function () {
+                removeWatchedMovie(movie.title);
+              }}
+            >
+              {movie.title}
+            </a>
+          </span>
+          <br />
+          <span>{movie.comment}</span>
+          <br />
+          <br />
         </div>
-        <span>
-          <a className="movie-watched" href="#" onClick={function() { removeWatchedMovie(movie.title) }}>
-            {movie.title}
-          </a>
-        </span>
-        <br />
-        <span>
-          {movie.comment}
-        </span>
-        <br />
-        <br />
-      </div>
-    ))
-  })
+      )
+    );
+  });
 
   return components;
 }
+
+/**
+ *
+ * This is not a best practice to define home page HTML code inside the Main App file.
+ * It should be defined in seperate file. e.g: HomePage.jsx or MainPage.jsx or in this case MoviesPAge.jsx
+ *
+ */
 
 function App(props) {
   return (
     <div className="App">
       <h1>Codest Movies!</h1>
       <h1>Add movie!</h1>
-      <b>TITLE:<br /><input type="text" onChange={function(e) { title = e.target.value; }} /></b><br />
-      <b>IMAGE URL:<br /><input type="text" onChange={function(e) { image = e.target.value; }} /></b><br />
-      <b>COMMENT:<br /><input type="text" onChange={function(e) { comment = e.target.value; }} /></b><br />
-      <input type="button" onClick={function(e) { add(title, image, comment); }} value="ADD MOVIE" />
+      {/* Should use HTML FORM ELEMENT here */}
+      <b>
+        TITLE:
+        <br />
+        <input
+          type="text"
+          onChange={function (e) {
+            title = e.target.value;
+          }}
+        />
+      </b>
+      <br />
+      <b>
+        IMAGE URL:
+        <br />
+        <input
+          type="text"
+          onChange={function (e) {
+            image = e.target.value;
+          }}
+        />
+      </b>
+      <br />
+      <b>
+        COMMENT:
+        <br />
+        <input
+          type="text"
+          onChange={function (e) {
+            comment = e.target.value;
+          }}
+        />
+      </b>
+      <br />
+      <input
+        type="button"
+        onClick={function (e) {
+          add(title, image, comment);
+        }}
+        value="ADD MOVIE"
+      />
 
       <h1>Watchlist:</h1>
       {getMoviesComponents(getAllMovies())}
@@ -76,8 +149,12 @@ function App(props) {
   );
 }
 
-var title = '';
-var image = '';
-var comment = '';
+/**
+ *
+ * Should use Controlled inputs with state.
+ */
+var title = "";
+var image = "";
+var comment = "";
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,89 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
+/**
+ * The Seperation of concerns is not followed here.
+ * This function should not be here. Better use a State Management approach
+ */
 export function getWatchedMovies() {
-	var movies = localStorage.getItem('movies-watched');
+  var movies = localStorage.getItem("movies-watched");
 
-	if (!movies) {
-		return [];
-	} else {
-		return JSON.parse(movies);
-	}
+  if (!movies) {
+    return [];
+  } else {
+    return JSON.parse(movies);
+  }
 }
 
+/**
+ * The Seperation of concerns is not followed here.
+ * This function should not be here. Better move this hard-coded data to a seperate file called data.js or api.js
+ */
 export function getAllMovies() {
-	var movies = localStorage.getItem('movies-all');
+  var movies = localStorage.getItem("movies-all");
 
-	if (!movies) {
-		return [
-		{
-			title: 'The Avengers',
-			image: 'http://d21lz9b0v8r1zn.cloudfront.net/wp-content/uploads//2012/03/detail.jpg',
-			comment: 'New York blows up in this!'
-		},
-		{
-			title: 'Dark City',
-			image: 'https://i.chzbgr.com/full/5569379584/hA96709E0/',
-			comment: 'This looks mysterious. Cool!'
-		},
-		{
-			title: 'Hot Tub Time Machine',
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG7vNmphIcVhEcybvSvMgbTkV6EE2twHBNanKvgDx3ZS7Ivn6Dtg',
-			comment: 'Someone said this was fun. Maybe!'
-		},
-		];
-	} else {
-		return JSON.parse(movies);
-	}
+  if (!movies) {
+    return [
+      {
+        title: "The Avengers",
+        image:
+          "http://d21lz9b0v8r1zn.cloudfront.net/wp-content/uploads//2012/03/detail.jpg",
+        comment: "New York blows up in this!",
+      },
+      {
+        title: "Dark City",
+        image: "https://i.chzbgr.com/full/5569379584/hA96709E0/",
+        comment: "This looks mysterious. Cool!",
+      },
+      {
+        title: "Hot Tub Time Machine",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG7vNmphIcVhEcybvSvMgbTkV6EE2twHBNanKvgDx3ZS7Ivn6Dtg",
+        comment: "Someone said this was fun. Maybe!",
+      },
+    ];
+  } else {
+    return JSON.parse(movies);
+  }
 }
 
+/**
+ *
+ * 1 - This function should not be here. Better use a State Management approach and add/delete/... should be a dispatch to the global state
+ * 2 - The name of the function should be more self-comentairy. e.g : addMovieToList
+ */
 export function add(title, description, image) {
-	var movie = {};
-	movie.title = title;
-	movie.description = description;
-	movie.image = image;
+  var movie = {};
+  movie.title = title;
+  movie.description = description;
+  movie.image = image;
 
-	var movies = getAllMovies();
-	movies.push(movie);
+  var movies = getAllMovies();
+  movies.push(movie);
 
-	localStorage.setItem('movies-all', JSON.stringify(movies));
+  localStorage.setItem("movies-all", JSON.stringify(movies));
 
-	render();
+  //   * This kind of re-rendering is not best practice and not efficient
+  render();
 }
 
+/**
+ *
+ * This function should not be here. Better use a State Management approach
+ */
 export function addWatchedMovie(title, description, image) {
-	var movie = {};
-	movie.title = title;
-	movie.description = description;
-	movie.image = image;
+  var movie = {};
+  movie.title = title;
+  movie.description = description;
+  movie.image = image;
 
-	var movies = getWatchedMovies();
-	movies.push(movie);
+  var movies = getWatchedMovies();
+  movies.push(movie);
 
-	localStorage.setItem('movies-watched', JSON.stringify(movies));
+  localStorage.setItem("movies-watched", JSON.stringify(movies));
 
-	render();
+  //* This kind of re-rendering is not best practice and not efficient
+  render();
 }
 
+/**
+ *
+ * This function should not be here. Better use a State Management approach
+ */
 export function removeWatchedMovie(title) {
-	var movies = getWatchedMovies();
+  var movies = getWatchedMovies();
 
-	for (var i = 0; i < movies.length; i++) {
-	   if (!movies[i]) continue;
-		if (movies[i].title == title) {
-			movies[i] = null
-		}
-	}
+  for (var i = 0; i < movies.length; i++) {
+    if (!movies[i]) continue;
+    // Better use ===. because it is safer checking type and value
+    if (movies[i].title == title) {
+      movies[i] = null;
+    }
+  }
 
-	localStorage.setItem('movies-watched', JSON.stringify(movies));
+  localStorage.setItem("movies-watched", JSON.stringify(movies));
 
-	render();
+  render();
 }
 
+/**
+ *
+ * This kind of re-rendering is not best practice and not efficient
+ */
 function render() {
-	ReactDOM.render(<App movies={getAllMovies()} watched={getWatchedMovies()} />, document.getElementById('root'))
+  ReactDOM.render(
+    <App movies={getAllMovies()} watched={getWatchedMovies()} />,
+    document.getElementById("root")
+  );
 }
 
 render();
